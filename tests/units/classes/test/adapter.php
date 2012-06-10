77a6df9b0ab48b1b0d869f39900b87272b6d6371
mageekguy <frederic.hardy@mageekbox.net>
2012-06-10 15:56:27 +0200

    Update unit tests.

--- tests/units/classes/test/adapter.php
+++ tests/units/classes/test/adapter.php
@@ -12,128 +12,113 @@ class adapter extends atoum\test
 {
 	public function test__construct()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$this->assert
-			->array($adapter->getInvokers())->isEmpty()
-			->array($adapter->getCalls())->isEmpty()
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->then
+				->array($adapter->getInvokers())->isEmpty()
+				->array($adapter->getCalls())->isEmpty()
 		;
 	}
 
 	public function test__set()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$closure = function() {};
-
-		$this->assert
-			->when(function() use ($adapter, & $closure) { $adapter->md5 = $closure = function() {}; })
-			->object($adapter->md5->getClosure())->isIdenticalTo($closure)
-		;
-
-		$adapter->md5 = $return = uniqid();
-
-		$this->assert
-			->object($adapter->md5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
-			->object($adapter->MD5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
-			->string($adapter->invoke('md5'))->isEqualTo($return)
-			->string($adapter->invoke('MD5'))->isEqualTo($return)
-		;
-
-		$adapter->MD5 = $return = uniqid();
-
-		$this->assert
-			->object($adapter->md5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
-			->object($adapter->MD5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
-			->string($adapter->invoke('md5'))->isEqualTo($return)
-			->string($adapter->invoke('MD5'))->isEqualTo($return)
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->and($adapter->md5 = $closure = function() {})
+			->then
+				->object($adapter->md5->getClosure())->isIdenticalTo($closure)
+			->if($adapter->md5 = $return = uniqid())
+			->then
+				->object($adapter->md5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
+				->string($adapter->invoke('md5'))->isEqualTo($return)
+				->object($adapter->MD5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
+				->string($adapter->invoke('MD5'))->isEqualTo($return)
+			->if($adapter->MD5 = $return = uniqid())
+			->then
+				->object($adapter->md5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
+				->string($adapter->invoke('md5'))->isEqualTo($return)
+				->object($adapter->MD5)->isInstanceOf('mageekguy\atoum\test\adapter\invoker')
+				->string($adapter->invoke('MD5'))->isEqualTo($return)
 		;
 	}
 
 	public function test__get()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$adapter->md5 = $closure = function() {};
-
-		$this->assert
-			->object($adapter->md5->getClosure())->isIdenticalTo($closure)
-			->object($adapter->MD5->getClosure())->isIdenticalTo($closure)
-		;
-
-		$adapter->md5 = uniqid();
-
-		$this->assert
-			->object($adapter->md5->getClosure())->isInstanceOf('closure')
-			->object($adapter->MD5->getClosure())->isInstanceOf('closure')
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->and($adapter->md5 = $closure = function() {})
+			->then
+				->object($adapter->md5->getClosure())->isIdenticalTo($closure)
+				->object($adapter->MD5->getClosure())->isIdenticalTo($closure)
+			->if($adapter->md5 = $return = uniqid())
+			->then
+				->object($adapter->md5->getClosure())->isInstanceOf('closure')
+				->object($adapter->MD5->getClosure())->isInstanceOf('closure')
 		;
 	}
 
 	public function test__isset()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$this->assert
-			->boolean(isset($adapter->md5))->isFalse()
-		;
-
-		$adapter->{$function = strtolower(uniqid())} = function() {};
-
-		$this->assert
-			->boolean(isset($adapter->{$function}))->isTrue()
-			->boolean(isset($adapter->{strtoupper($function)}))->isTrue()
-		;
-
-		$adapter->{$function = strtoupper(uniqid())} = function() {};
-
-		$this->assert
-			->boolean(isset($adapter->{$function}))->isTrue()
-			->boolean(isset($adapter->{strtolower($function)}))->isTrue()
-		;
-
-		$adapter->{$function = strtolower(uniqid())} = uniqid();
-
-		$this->assert
-			->boolean(isset($adapter->{$function}))->isTrue()
-			->boolean(isset($adapter->{strtoupper($function)}))->isTrue()
-		;
-
-		$adapter->{$function = strtoupper(uniqid())} = uniqid();
-
-		$this->assert
-			->boolean(isset($adapter->{$function}))->isTrue()
-			->boolean(isset($adapter->{strtolower($function)}))->isTrue()
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->then
+				->boolean(isset($adapter->md5))->isFalse()
+			->if($adapter->{$function = strtolower(uniqid())} = function() {})
+			->then
+				->boolean(isset($adapter->{$function}))->isTrue()
+				->boolean(isset($adapter->{strtoupper($function)}))->isTrue()
+			->if($adapter->{$function = strtoupper(uniqid())} = function() {})
+			->then
+				->boolean(isset($adapter->{$function}))->isTrue()
+				->boolean(isset($adapter->{strtolower($function)}))->isTrue()
+			->if($adapter->{$function = strtolower(uniqid())} = uniqid())
+			->then
+				->boolean(isset($adapter->{$function}))->isTrue()
+				->boolean(isset($adapter->{strtoupper($function)}))->isTrue()
+			->if($adapter->{$function = strtoupper(uniqid())} = uniqid())
+			->then
+				->boolean(isset($adapter->{$function}))->isTrue()
+				->boolean(isset($adapter->{strtolower($function)}))->isTrue()
 		;
 	}
 
 	public function test__unset()
 	{
-		$this->assert
+		$this
 			->when(function() use (& $adapter) { $adapter = new atoum\test\adapter(); })
+			->then
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isEmpty()
 			->when(function() use ($adapter) { unset($adapter->md5); })
+			->then
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isEmpty()
 			->when(function() use ($adapter) { unset($adapter->MD5); })
+			->then
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isEmpty()
 			->when(function() use ($adapter) { $adapter->md5 = uniqid(); $adapter->md5(uniqid()); })
+			->then
 				->array($adapter->getInvokers())->isNotEmpty()
 				->array($adapter->getCalls())->isNotEmpty()
 			->when(function() use ($adapter) { unset($adapter->{uniqid()}); })
+			->then
 				->array($adapter->getInvokers())->isNotEmpty()
 				->array($adapter->getCalls())->isNotEmpty()
 			->when(function() use ($adapter) { unset($adapter->md5); })
+			->then
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isEmpty()
 			->when(function() use ($adapter) { $adapter->MD5 = uniqid(); $adapter->MD5(uniqid()); })
+			->then
 				->array($adapter->getInvokers())->isNotEmpty()
 				->array($adapter->getCalls())->isNotEmpty()
 			->when(function() use ($adapter) { unset($adapter->{uniqid()}); })
+			->then
 				->array($adapter->getInvokers())->isNotEmpty()
 				->array($adapter->getCalls())->isNotEmpty()
 			->when(function() use ($adapter) { unset($adapter->MD5); })
+			->then
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isEmpty()
 		;
@@ -141,113 +126,77 @@ class adapter extends atoum\test
 
 	public function test__call()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$this->assert
-			->string($adapter->md5($hash = uniqid()))->isEqualTo(md5($hash))
-			->string($adapter->MD5($hash = uniqid()))->isEqualTo(md5($hash))
-		;
-
-		$adapter->md5 = $md5 = uniqid();
-
-		$this->assert
-			->string($adapter->md5($hash))->isEqualTo($md5)
-			->string($adapter->MD5($hash))->isEqualTo($md5)
-		;
-
-		$adapter->md5 = $md5 = uniqid();
-
-		$this->assert
-			->string($adapter->md5($hash))->isEqualTo($md5)
-			->string($adapter->MD5($hash))->isEqualTo($md5)
-		;
-
-		$this->assert
-			->exception(function() use ($adapter) {
-						$adapter->require(uniqid());
-					}
-				)
-				->isInstanceOf('mageekguy\atoum\exceptions\logic\invalidArgument')
-				->hasMessage('Function \'require()\' is not invokable by an adapter')
-			->exception(function() use ($adapter) {
-						$adapter->REQUIRE(uNiqid());
-					}
-				)
-				->isInstanceOf('mageekguy\atoum\exceptions\logic\invalidArgument')
-				->hasMessage('Function \'REQUIRE()\' is not invokable by an adapter')
-		;
-
-		$adapter->md5 = 0;
-		$adapter->md5[1] = 1;
-		$adapter->md5[2] = 2;
-
-		$this->assert
-			->integer($adapter->md5())->isEqualTo(1)
-			->integer($adapter->md5())->isEqualTo(2)
-			->integer($adapter->md5())->isEqualTo(0)
-		;
-
-		$this->assert
-			->integer($adapter->MD5())->isEqualTo(1)
-			->integer($adapter->MD5())->isEqualTo(2)
-			->integer($adapter->MD5())->isEqualTo(0)
-		;
-
-		$adapter->MD5 = 0;
-		$adapter->MD5[1] = 1;
-		$adapter->MD5[2] = 2;
-
-		$this->assert
-			->integer($adapter->md5())->isEqualTo(1)
-			->integer($adapter->md5())->isEqualTo(2)
-			->integer($adapter->md5())->isEqualTo(0)
-		;
-
-		$this->assert
-			->integer($adapter->MD5())->isEqualTo(1)
-			->integer($adapter->MD5())->isEqualTo(2)
-			->integer($adapter->MD5())->isEqualTo(0)
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->then
+				->string($adapter->md5($hash = uniqid()))->isEqualTo(md5($hash))
+				->string($adapter->MD5($hash = uniqid()))->isEqualTo(md5($hash))
+			->if($adapter->md5 = $md5 = uniqid())
+			->then
+				->string($adapter->md5($hash))->isEqualTo($md5)
+				->string($adapter->MD5($hash))->isEqualTo($md5)
+				->exception(function() use ($adapter) {
+							$adapter->require(uniqid());
+						}
+					)
+					->isInstanceOf('mageekguy\atoum\exceptions\logic\invalidArgument')
+					->hasMessage('Function \'require()\' is not invokable by an adapter')
+				->exception(function() use ($adapter) {
+							$adapter->REQUIRE(uNiqid());
+						}
+					)
+					->isInstanceOf('mageekguy\atoum\exceptions\logic\invalidArgument')
+					->hasMessage('Function \'REQUIRE()\' is not invokable by an adapter')
+			->if($adapter->md5 = 0)
+			->and($adapter->md5[1] = 1)
+			->and($adapter->md5[2] = 2)
+			->and($adapter->resetCalls())
+			->then
+				->integer($adapter->md5())->isEqualTo(1)
+				->integer($adapter->md5())->isEqualTo(2)
+				->integer($adapter->md5())->isEqualTo(0)
+			->if($adapter->MD5 = 0)
+			->and($adapter->MD5[1] = 1)
+			->and($adapter->MD5[2] = 2)
+			->and($adapter->resetCalls())
+			->then
+				->integer($adapter->md5())->isEqualTo(1)
+				->integer($adapter->md5())->isEqualTo(2)
+				->integer($adapter->md5())->isEqualTo(0)
 		;
 	}
 
 	public function testGetCallsNumber()
 	{
-		$this->assert
+		$this
 			->integer(atoum\test\adapter::getCallsNumber())->isZero()
-		;
-
-		$adapter = new atoum\test\adapter();
-		$adapter->md5(uniqid());
-
-		$this->assert
-			->integer(atoum\test\adapter::getCallsNumber())->isEqualTo(1)
-		;
-
-		$adapter->md5(uniqid());
-
-		$this->assert
-			->integer(atoum\test\adapter::getCallsNumber())->isEqualTo(2)
-		;
-
-		$otherAdapter = new atoum\test\adapter();
-		$otherAdapter->sha1(uniqid());
-
-		$this->assert
-			->integer(atoum\test\adapter::getCallsNumber())->isEqualTo(3)
+			->if($adapter = new atoum\test\adapter())
+			->and($adapter->md5(uniqid()))
+			->then
+				->integer(atoum\test\adapter::getCallsNumber())->isEqualTo(1)
+			->if($adapter->md5(uniqid()))
+			->then
+				->integer(atoum\test\adapter::getCallsNumber())->isEqualTo(2)
+			->if($otherAdapter = new atoum\test\adapter())
+			->and($otherAdapter->sha1(uniqid()))
+			->then
+				->integer(atoum\test\adapter::getCallsNumber())->isEqualTo(3)
 		;
 	}
 
 	public function testGetCalls()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$this->assert
-			->array($adapter->getCalls())->isEmpty()
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->then
+				->array($adapter->getCalls())->isEmpty()
 			->when(function() use ($adapter, & $firstHash) { $adapter->md5($firstHash = uniqid()); })
+			->then
 				->array($adapter->getCalls())->isEqualTo(array('md5' => array(1 => array($firstHash))))
 				->array($adapter->getCalls('md5'))->isEqualTo(array(1 => array($firstHash)))
 				->array($adapter->getCalls('MD5'))->isEqualTo(array(1 => array($firstHash)))
 			->when(function() use ($adapter, & $secondHash) { $adapter->md5($secondHash = uniqid()); })
+			->then
 				->array($adapter->getCalls())->isEqualTo(array('md5' => array(1 => array($firstHash), 2 => array($secondHash))))
 				->array($adapter->getCalls('md5'))->isEqualTo(array(1 => array($firstHash), 2 => array($secondHash)))
 				->array($adapter->getCalls('MD5'))->isEqualTo(array(1 => array($firstHash), 2 => array($secondHash)))
@@ -256,6 +205,7 @@ class adapter extends atoum\test
 					$adapter->md5($thirdHash = uniqid());
 				}
 			)
+			->then
 				->array($adapter->getCalls())->isEqualTo(array('md5' => array(1 => array($firstHash), 2 => array($secondHash), 3 => array($thirdHash))))
 				->array($adapter->getCalls('md5'))->isEqualTo(array(1 => array($firstHash), 2 => array($secondHash), 3 => array($thirdHash)))
 				->array($adapter->getCalls('MD5'))->isEqualTo(array(1 => array($firstHash), 2 => array($secondHash), 3 => array($thirdHash)))
@@ -267,6 +217,7 @@ class adapter extends atoum\test
 					$adapter->strpos($haystack, $needle, $offset);
 				}
 			)
+			->then
 				->array($adapter->getCalls())->isEqualTo(array(
 							'md5' => array(
 								1 => array($firstHash),
@@ -287,10 +238,10 @@ class adapter extends atoum\test
 
 	public function testResetCalls()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$this->assert
-			->when(function() use ($adapter) { $adapter->md5(uniqid()); })
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->and($adapter->md5(uniqid()))
+			->then
 				->array($adapter->getCalls())->isNotEmpty()
 				->object($adapter->resetCalls())->isIdenticalTo($adapter)
 				->array($adapter->getCalls())->isEmpty()
@@ -299,31 +250,31 @@ class adapter extends atoum\test
 
 	public function testReset()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$this->assert
-			->array($adapter->getInvokers())->isEmpty()
-			->array($adapter->getCalls())->isEmpty()
-			->object($adapter->reset())->isIdenticalTo($adapter)
-			->array($adapter->getInvokers())->isEmpty()
-			->array($adapter->getCalls())->isEmpty()
-			->when(function() use ($adapter) { $adapter->md5(uniqid()); })
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->then
+				->array($adapter->getInvokers())->isEmpty()
+				->array($adapter->getCalls())->isEmpty()
+				->object($adapter->reset())->isIdenticalTo($adapter)
+				->array($adapter->getInvokers())->isEmpty()
+				->array($adapter->getCalls())->isEmpty()
+			->if($adapter->md5(uniqid()))
+			->then
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isNotEmpty()
 				->object($adapter->reset())->isIdenticalTo($adapter)
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isEmpty()
-			->when(function() use ($adapter) { $adapter->md5 = uniqid(); })
+			->if($adapter->md5 = uniqid())
+			->then
 				->array($adapter->getInvokers())->isNotEmpty()
 				->array($adapter->getCalls())->isEmpty()
 				->object($adapter->reset())->isIdenticalTo($adapter)
 				->array($adapter->getInvokers())->isEmpty()
 				->array($adapter->getCalls())->isEmpty()
-			->when(function() use ($adapter) {
-					$adapter->md5 = uniqid();
-					$adapter->md5(uniqid());
-				}
-			)
+			->if($adapter->md5 = uniqid())
+			->and($adapter->md5(uniqid()))
+			->then
 				->array($adapter->getInvokers())->isNotEmpty()
 				->array($adapter->getCalls())->isNotEmpty()
 				->object($adapter->reset())->isIdenticalTo($adapter)
@@ -334,13 +285,13 @@ class adapter extends atoum\test
 
 	public function testAddCall()
 	{
-		$adapter = new atoum\test\adapter();
-
-		$this->assert
-			->array($adapter->getCalls())->isEmpty()
-			->object($adapter->addCall($method = uniqid(), $args = array(uniqid())))->isIdenticalTo($adapter)
-			->array($adapter->getCalls($method))->isEqualTo(array(1 => $args))
-			->array($adapter->getCalls(strtoupper($method)))->isEqualTo(array(1 => $args))
+		$this
+			->if($adapter = new atoum\test\adapter())
+			->then
+				->array($adapter->getCalls())->isEmpty()
+				->object($adapter->addCall($method = uniqid(), $args = array(uniqid())))->isIdenticalTo($adapter)
+				->array($adapter->getCalls($method))->isEqualTo(array(1 => $args))
+				->array($adapter->getCalls(strtoupper($method)))->isEqualTo(array(1 => $args))
 		;
 	}
 }
