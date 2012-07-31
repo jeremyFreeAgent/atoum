776ceb806313ca8368bbecf6630933fb65676865
mageekguy <frederic.hardy@mageekbox.net>
2012-07-31 13:36:33 +0200

    Cleaning.

--- tests/units/classes/adapter.php
+++ tests/units/classes/adapter.php
@@ -10,21 +10,6 @@ require_once __DIR__ . '/../runner.php';
 
 class adapter extends atoum\test
 {
-	/** @engine inline */
-	public function testFpm()
-	{
-		$client = new atoum\fcgi\client('127.0.0.1', '9000');
-		$request = new atoum\fcgi\request();
-		$request->stdin = 'query=1234';
-		$request->gateway_interface = 'FastCGI/1.0';
-		$request->request_method = 'POST';
-		$request->script_filename = '/Users/fch/tmp/post.php';
-		$request->content_type = 'application/x-www-form-urlencoded';
-		$request->content_length = strlen($request->stdin);
-		var_dump($request($client));
-	}
-
-	/*
 	public function test__construct()
 	{
 		$this
@@ -42,5 +27,4 @@ class adapter extends atoum\test
 				->string($adapter->md5($hash = uniqid()))->isEqualTo(md5($hash))
 		;
 	}
-	*/
 }
